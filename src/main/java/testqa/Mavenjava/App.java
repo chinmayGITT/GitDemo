2447399c3873358025b52377d5cfde6af896cf62
chinmayGITT <chinmay.frrm@gmail.com>
2021-05-31 14:08:06 +0530

    First Commit

---
+++ src/main/java/testqa/Mavenjava/App.java
@@ -0,0 +1,13 @@
+package testqa.Mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
